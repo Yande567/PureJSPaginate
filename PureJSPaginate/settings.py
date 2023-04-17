0dc6f791b4f632db90ed71a532ee96107bc49ed5
Yande567 <ymusonda2001@gmail.com>
2023-04-17 09:21:46 +0200

    Removed Secret Keys

--- PureJSPaginate/settings.py
+++ PureJSPaginate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qpli&-v78&jw3w%r((cfyipoh_-9fa92l_rxy1^tskk#n)od5v"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
